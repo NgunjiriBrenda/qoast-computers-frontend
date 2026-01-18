263fffc0adf55405979878458fd12d807fd62202
brenda njoki <ngunjiribrenda5@gmail.com>
2026-01-18 23:10:15 +0300

    Fetches products

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if (localStorage.getItem("isLoggedIn") !== "true"){
 
 const servicesContainer = document.getElementById("service-list");
 
-fetch("http://localhost:3000/services"
+fetch("http://localhost:3000/services")
     .then(res => res.json())
     .then(services =>{
         servicesContainer.innerHTML = ""
@@ -25,4 +25,30 @@ fetch("http://localhost:3000/services"
         servicesContainer.innerHTML = "<p>Failed to load services.</p>";
         console.error(err);
     })
-)
+
+
+const productContainer = document.getElementById("product-list");
+
+fetch("http://localhost:3000/product")
+.then(res => res.json())
+.then(products => {
+    productContainer.innerHTML = "";
+
+    products.forEach(product => {
+        const card = document.createElement("div");
+        card.className ="card";
+
+        card.innerHTML=`
+        <img src="${product.image}" alt="${product.name}
+        <h3>${product.name}</h3>
+        <p>${product.price}</p>
+        `;
+
+        productContainer.append(card);
+    });
+})
+
+.catch(err => {
+    productContainer.innerHTML = "<p>Failed to load products.</p>";
+    console.error(err);
+});
