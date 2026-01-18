c7aa2d3e943554809b455ee1bba1e9bb5069142e
brenda njoki <ngunjiribrenda5@gmail.com>
2026-01-18 22:46:44 +0300

    Fetches services

--- index.js
+++ index.js
@@ -2,3 +2,27 @@ if (localStorage.getItem("isLoggedIn") !== "true"){
     window.location.href = "login.html";
 }
 
+const servicesContainer = document.getElementById("service-list");
+
+fetch("http://localhost:3000/services"
+    .then(res => res.json())
+    .then(services =>{
+        servicesContainer.innerHTML = ""
+
+        services.forEach(service => {
+            const card = document.createElement("div");
+            card.className="card";
+
+            card.innerHTML=`
+            <h3>${services.title}</h3>
+            <p>${services.description}</p>
+            `;
+
+            servicesContainer.appendChild(card);
+        });
+    })
+    .catch(err => {
+        servicesContainer.innerHTML = "<p>Failed to load services.</p>";
+        console.error(err);
+    })
+)
